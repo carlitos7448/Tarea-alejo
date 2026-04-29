3b67c8be8f06ad61effdcf19e9d5a362cc8439c7
carlos <carlosrive367@gmail.com>
2026-04-28 23:19:46 -0500

    feat: implementar conversor de unidades tipado

--- Ejercicio-9.ts
+++ Ejercicio-9.ts
@@ -3,3 +3,17 @@ Diseña una función que permita convertir entre distintas unidades de medida:
 "cm", "m" y "km".
 Usa un tipo unión para representar las unidades posibles y asegúrate de validar
 correctamente los valores de entrada y salida.*/
+
+
+type Unidad = "cm" | "m" | "km";
+
+const convertirUnidades = (valor: number, desde: Unidad, a: Unidad): number => {
+  const factores: Record<Unidad, number> = {
+    cm: 0.01,
+    m: 1,
+    km: 1000
+  };
+
+  const valorEnMetros = valor * factores[desde];
+  return valorEnMetros / factores[a];
+};
